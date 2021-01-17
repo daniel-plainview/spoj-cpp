4727e7f8f7b2bcb7092126c1fb093e5442ceb0ca
Maxwell Tyler <mukundsrinivasb@hotmail.com>
2021-01-17 23:58:48 +0530

    An attempt was made to make this use pointers, take a through tomorrow

--- C++/first4pows_dp_experiment.cpp
+++ C++/first4pows_dp_experiment.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 #include<bits/stdc++.h>
-#include<vector>
 using namespace std;
-int first4pow(int base, int index )
+int* first4pow(int base, int index )
 {
-  vector <int> repowers(5);
+  int repowers[5];
   repowers[0]=1;
   int base_units=base % 10, multi = 1  ;
   for(int i=1;i<=4;i++)
@@ -12,39 +11,21 @@ int first4pow(int base, int index )
     multi = base_units * repowers [i-1];
     repowers[i] = multi % 10;
   }
-  if(base%10)
-  {
-    if(index%4)
-    {
-      return repowers[index % 4];
-    }
-    else
-    {
-      return repowers[4];
-    }
-  }
-  else
-  {
-    if(index)
-    {
-      return 0;
-    }
-    else
-    {
-      return 1;
-    }
-  }
+  return *repowers;
 }
-
 int main()
 {
-  int base,expo,trial,output;
+  int base,expo,trial
+  int *output;
   cin>>trial;
   while(trial--)
   {
     cin>>base>>expo;
     output=first4pow(base,expo);
-    cout<<output<<endl;
+    for(int i=0;i<5;i++)
+    {
+    cout<<*(output+i)<<endl;
+    }
   }
   return 0;
 }
