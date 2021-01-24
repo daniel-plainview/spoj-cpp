f243fefb3b51ecbd53b71325a70f45f9dfc15fb8
Maxwell Tyler <mukundsrinivasb@hotmail.com>
2021-01-24 21:20:00 +0530

    Added the correct solutions to the problems

--- C++/Things_that_were_wrong/AE00_experiment.cpp
+++ C++/Things_that_were_wrong/AE00_experiment.cpp
@@ -1,21 +1,39 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int count_factors(int a)
 {
-  int numrec=0;
-  for(int j=1;j<=a;j++)
+  vector <int> lista;
+  vector <int> listb;
+  if(a==1)
   {
-    int count=0;
-    for(int i=1;i*i<=j;i++)
-    {
-      if(j%i==0)
-      count++;
-    }
-    numrec+=count;
+    return 0;
   }
-return numrec;
+  else
+    {
+    for(int i=1;i*i<=a;i++)
+    {
+      if(a%i==0)
+      {
+        lista.push_back(i);
+        listb.push_back(a/i);
+      }
+    }
+    int count=0;
+    for(int i=0;i<lista.size();i++)
+    {
+      if(lista[i]==listb[i])
+      {
+        count+=1;
+      }
+      else
+      {
+        count+=2;
+      }
+    }
+    return count;
+  }
 }
-
 int main()
 {
   int input;
