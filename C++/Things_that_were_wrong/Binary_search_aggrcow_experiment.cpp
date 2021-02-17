25e29d3bd647169243297e4edfbed11d2e5ad54b
Maxwell Tyler <mukundsrinivasb@hotmail.com>
2021-02-18 00:23:51 +0530

    These changes were not Accepted

--- C++/Things_that_were_wrong/Binary_search_aggrcow_experiment.cpp
+++ C++/Things_that_were_wrong/Binary_search_aggrcow_experiment.cpp
@@ -7,18 +7,34 @@ using namespace std;
 int largestdist(vector <int> poslist ,int n ,int ncows)
 {
   sort(poslist.begin(),poslist.end());
+<<<<<<< HEAD:C++/Things_that_were_wrong/Binary_search_aggrcow_experiment.cpp
   int low=poslist[0],high=poslist[n-1],cow=1,left=0,best=0,gap;
   while(low<=high)
   {
     gap=(low+high-1)/2;
     for(int i=1;i<=n && cow<ncows;i++)
+=======
+  int low=poslist.front(),high=poslist.back(),gap=(low+high)/2;
+  int cow=1,left=0,i=1;
+  while(low<=high)
+  {
+    for(;i<=n && cow<=ncows; i++)
+>>>>>>> 71dcf774abb38f5a813c478c916e2c83190a390f:C++/Things_that_were_wrong/Binary_sort_experimentation.cpp
     {
       if(poslist[i]-poslist[left] >= gap)
       {
+        int temp;
         cow++;
-        left=i;
+        temp=i;
+        i=left+1;
+        left=temp;
+        if(poslist[i] + gap > poslist.back())
+        {
+          high=gap;
+        }
       }
     }
+<<<<<<< HEAD:C++/Things_that_were_wrong/Binary_search_aggrcow_experiment.cpp
     if(cow>=ncows)
     {
         best=cow;
@@ -29,8 +45,11 @@ int largestdist(vector <int> poslist ,int n ,int ncows)
     {
         high = gap-1;
     }
+=======
+    gap=low+high/2;
+>>>>>>> 71dcf774abb38f5a813c478c916e2c83190a390f:C++/Things_that_were_wrong/Binary_sort_experimentation.cpp
   }
-  return best;
+  return i;
 }
 
 int main()
