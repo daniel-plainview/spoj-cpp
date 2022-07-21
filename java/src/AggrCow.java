493a024279c3a4281b9018c60d373a6f5ece579a
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2022-07-21 22:52:49 +1000

    Some Changes to AggrCow

--- java/src/AggrCow.java
+++ java/src/AggrCow.java
@@ -1,66 +1,36 @@
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 
 public class AggrCow {
-    private static ArrayList<Integer> inputOfNumbers = new ArrayList<>();
-    private static Integer MIN, MAX;
-    private static final HashMap<Integer, Boolean> positionMap = new HashMap<>();
-    private static AggrCow instance;
+    public int n, c;
+    public ArrayList<Integer> positions;
+    private int[] orderedPositions;
 
-    AggrCow(ArrayList<Integer> consoleInput) {
-        try {
-            for (Integer position : positionMap.keySet()) {
-
-                if (!positionMap.get(position)) {
-
-                }
-
+    public AggrCow(int n, int c, ArrayList<Integer> positions) {
+        int[] pos = new int[positions.size()];
+        Arrays.sort(pos);
+        setOrderedPositions(pos);
+        int low = pos[1] - pos[0];
+        int high = pos[pos.length - 1] - pos[0];
+        while (low < high) {
+            int mid = (high + low) / 2;
+            //if cows can be placed at a distance of mid
+            if (canPlace(mid)) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-        } catch (NullPointerException e) {
-            instance = getInstance(consoleInput);
-
-
         }
-    }
-
-    private AggrCow getInstance(ArrayList<Integer> consoleInput) {
-        if (instance == null) {
-            int min = MAX, max = MIN;
-            for (Integer in : consoleInput) {
-                min = Math.min(min, in);
-                max = Math.max(max, in);
-
-            }
-            MAX = max;
-            MIN = min;
-            setPositionMap(consoleInput);
-
-            return new AggrCow(consoleInput);
-        } else return instance;
 
     }
 
-    private int recPositionMap(Integer position) {
-        if (position.equals(MIN) || position.equals(MAX))
-            return position;
-        else {
-            //Return the next position to be taken by the stall
-            // if not placed , check if its current position is the least from left
-            if(positionMap.get(position))
-                return position;
-            else
-
-        }
+    public boolean canPlace(int distance) {
+        int counter = 1;
+        int nextPos = positions.get(0) + distance;
     }
 
-
-    public void setPositionMap(ArrayList<Integer> inputValues) {
-        for (Integer i : inputValues) {
-            positionMap.put(i, false);
-        }
-        inputOfNumbers = inputValues;
+    public void setOrderedPositions(int[] orderedPositions) {
+        this.orderedPositions = orderedPositions;
     }
-
-
 }
